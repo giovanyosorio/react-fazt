1e6ebb67bdf7601d4859ab8f18fd080c447a20e0
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-17 16:20:29 +0000

    eliminado elementos

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,19 @@
 
 
 function TaskCard({task}) {
+
+    function eliminarTarea(){
+       //alert(task.id)
+       
+    }
     return(
         <div>
         <h1>{task.title}</h1>
         <p>{task.description}</p>
+        <button
+        onClick={eliminarTarea}>Eliminar tarea
+
+        </button>
     </div>
     )
 }

9eddf0524a4e50060cdf7d8937a80c051a2c7a6e
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-17 17:58:24 +0000

    creando el contexto

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -4,14 +4,21 @@ function TaskCard({task,deleteTask}) {
 
 
     return(
-        <div>
-        <h1>{task.title}</h1>
-        <p>{task.description}</p>
-        <button
-        onClick={()=>deleteTask(task.id)}>Eliminar tarea
 
-        </button>
-    </div>
+        
+                
+            
+                <div>
+                <h1>{task.title}</h1>
+                <p>{task.description}</p>
+                <button
+                onClick={()=>deleteTask(task.id)}>Eliminar tarea
+
+                </button>
+            </div>
+         
+       
+
     )
 }
 

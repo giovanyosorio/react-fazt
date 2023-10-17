088d4b48f926c9b7bcd9f39909f06fa7ad83a690
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-17 18:04:17 +0000

    crear contexto

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,12 @@
 
-
+import {useContext} from 'react'
+import {TaskContext} from '../context/TaskContext'
 function TaskCard({task,deleteTask}) {
 
-
+const valor=useContext(TaskContext)
+console.log(valor);
     return(
 
-        
-                
-            
                 <div>
                 <h1>{task.title}</h1>
                 <p>{task.description}</p>
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -4,8 +4,10 @@ import { createContext } from "react"
 export const TaskContext=createContext()
 
  export function TaskContextProvider(props) {
+
+    let x=20
   return (
-        <TaskContext.Provider>
+        <TaskContext.Provider value={x}>
             {props.children}
         </TaskContext.Provider>
   )
